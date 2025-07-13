d236ca378b1fbf1829b425f13c29b1a8233a7f36
astra-the-boop <astra.the.boop@icloud.com>
2025-07-13 18:52:45 +0700

    fuck yeah made an actually functioning demo (for now it's just the time thingys but i'll implement a better front-end soon enough :P)

--- server.js
+++ server.js
@@ -2,34 +2,42 @@ const express = require("express");
 const axios = require("axios");
 const dotenv = require("dotenv");
 const path = require("path");
+const ejs = require("ejs");
 
 
 dotenv.config();
 const app = express();
 
 app.set("view engine", "ejs");
-app.use("views", path.join(__dirname), "views");
+app.set("views", path.join(__dirname, "views"));
 
 const getData = async () => {
     try{
         const res = await axios.get(
-            "https://hackatime.hackclub.com/api/hackatime/v1",
+            `https://hackatime.hackclub.com/api/v1/users/my/stats?api_key=${process.env.APIKEY}`,
             {
                 headers: {
-                    Authorization: `Basic ${Buffer.from(process.env.APIKEY).toString("base64")}`,
+                    Authorization: `Bearer ${process.env.APIKEY}`,
                 },
             }
         );
+
+        return res.data;
     }catch(err){
-        console.error(err.message);
+        if (err.response){
+            console.error("hackatime error ", err.message.status, err.response.data);
+        }else{
+            console.error("network/api error ", err.message);
+        }
         return null;
     }
 };
 
 app.get("/", async(req, res) => {
     const data = await getData();
+    console.log(data);
     if (!data) return res.status(500).send("Failed to load HackaTime data :(")
-    res.render("index", {waka: data});
+    res.render("index", {waka: data.data});
 });
 
 const PORT = process.env.PORT || 3000;
